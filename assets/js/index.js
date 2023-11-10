cc52b75ee34828d329d49f8fdffd8401ecdc467d
Ricardo Ramirez <ricardoram98alv@gmail.com>
2023-11-10 01:30:35 -0500

    nuevorepositorio

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+//dom modelo de objetos del documento 
+// console.log('DOM:')
+// console.log(window)
+// window.alert('Welcome')
+// const value = window.prompt('Cual es tu nombre')
+// console.log('hola' + value);
+// estos metodos era utilizados antes para traer info
+
+// const value = window.confirm('Quieres ir a mi portafolio')
+// console.log(value)
+// if (value) {
+//     window.location.href="https://www.google.com.ec/?hl=es"
+// }
+// console.log(window.navigator.userAgent)
+// console.log(window.navigator.userAgent.split(' '))
+
+//DOM
+// console.log(window.document)
+console.dir(document)
+//html
+console.dir(document.documentElement)
+//head
+console.log(document.head)
+
+
